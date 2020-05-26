f7b537a680d6be82dda8a8954245829bff8999a6
wojtek <doliwojtek@gmail.com>
2020-05-26 11:19:21 +0200

    sorting countries alphabetically

--- src/app/shared/services/settings/settings.service.ts
+++ src/app/shared/services/settings/settings.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { CountryModel } from '../../models/country.model';
+import { map } from 'rxjs/operators';
 
 @Injectable({
     providedIn: 'root',
@@ -13,7 +14,13 @@ export class SettingsService {
 
     // Get List Of Countries
     fetchCountries(): Observable<CountryModel[]> {
-        return this.http.get<CountryModel[]>(this.#apiUrl + `countries`);
+        return this.http.get<CountryModel[]>(this.#apiUrl + `countries`)
+            .pipe(
+                map((data: CountryModel[]) => {
+                    data.sort((a, b) => (a.Country > b.Country) ? 1 : -1);
+                    return data;
+                })
+            );
     }
 
 }
