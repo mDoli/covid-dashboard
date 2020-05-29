21ea345486d37bfb6f1ef09a3349bd5f8732e7c6
wojtek <doliwojtek@gmail.com>
2020-05-29 12:50:53 +0200

    styling code

--- src/app/main/theme-switch/theme-switch.component.ts
+++ src/app/main/theme-switch/theme-switch.component.ts
@@ -1,18 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-theme-switch',
   templateUrl: './theme-switch.component.html',
   styleUrls: ['./theme-switch.component.scss']
 })
-export class ThemeSwitchComponent implements OnInit {
+export class ThemeSwitchComponent {
   darkTheme = true;
-  constructor() { }
 
-  ngOnInit() {
-  }
-
-  changeTheme() {
+  changeTheme(): void {
     this.darkTheme = !this.darkTheme;
     if (document.body.className === 'mat-typography mat-app-background dark-theme') {
       document.body.className = 'mat-typography mat-app-background light-theme';
